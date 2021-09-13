8bc3b7d319145ed42a57e78264010933cd41a8cf
maddenlockin <maddenlockin@gmail.com>
2021-09-13 16:16:29 -0700

    copyAndPush green with push

---
+++ copy-and-push.js
@@ -0,0 +1,8 @@
+/* eslint-disable indent */
+const copyAndPush = (arr, newItem) => {
+    const newArr = arr;
+    newArr.push(newItem);
+    return newArr;
+};
+
+export default copyAndPush;
---
+++ copy-and-push.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable indent */
+
+import copyAndPush from './copy-and-push';
+
+describe('copyAndPush', () => {
+    it('returns a new array with all the items in the original array and a new item pushed to the end', () => {
+        const numbers = [1, 2, 3];
+        const actual = copyAndPush(numbers, 4);
+        const expected = [1, 2, 3, 4];
+
+        expect(actual).toEqual(expected);
+
+    });
+});
