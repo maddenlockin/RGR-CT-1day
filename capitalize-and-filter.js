c9e80da4457fde1602c84af46fb42b9b38c62633
maddenlockin <maddenlockin@gmail.com>
2021-09-13 17:36:17 -0700

    capitalizeAndFilter for loop function passing

--- capitalize-and-filter.js
+++ capitalize-and-filter.js
@@ -1,6 +1,12 @@
 /* eslint-disable indent */
 
-const capitalizeAndFilter = () => {
-
+const capitalizeAndFilter = (arr) => {
+    const newArr = [];
+    for (const obj of arr) {
+        if (obj.charAt(0) !== 'f' && obj.charAt(0) !== 'F') {
+            newArr.push(obj.toUpperCase());
+        }
+    }
+    return newArr;
 };
 export default capitalizeAndFilter;
--- capitalize-and-filter.test.js
+++ capitalize-and-filter.test.js
@@ -5,7 +5,7 @@ describe ('capitalizeAndFilter', () => {
     it('takes an array of strings capitalize all strings and filter out any string that starts with the letter F/f', () => {
         const arr = ['toe', 'foot', 'Funny bone', 'hand'];
         const actual = capitalizeAndFilter(arr);
-        const expected = ['Toe', 'Hand'];
+        const expected = ['TOE', 'HAND'];
 
         expect(actual).toEqual(expected);
     });
